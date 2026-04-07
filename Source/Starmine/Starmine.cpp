5fc0aefcf23cf779b7aa7368ec190991d8a611ce
murnana <murnana@users.noreply.github.com>
2026-04-08 07:59:09 +0900

    :tada: Initial Unreal Engine project

---
+++ Source/Starmine/Starmine.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Starmine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Starmine, "Starmine" );
